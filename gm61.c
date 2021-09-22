7825c6e396d72736502dfd2cbd8fb83382b59b15
ville <ville.kaivola@gmail.com>
2021-09-22 19:33:57 +0300

    changed output formatting + some refactoring

--- gm61.c
+++ gm61.c
@@ -6,6 +6,15 @@
 int scanner;
 char data;
 
+int INIT_SCANNER[][2] = {
+    {0x00, 0x82}, // continous reading on and light normally off
+    {0x05, 0x05}, // 0.5 second read interval
+    {0x13, 0x94}, // 2 second same barcode read interval
+    {0x2C, 0x02}, // read all types of barcodes
+    {0x2E, 0x01}, // enable reading EAN13, because the above doesn't do it
+    {0x60, 0x21}, // add newline after the scanned value
+};
+
 int main() {
     if (wiringPiSetup() < 0) return -1;
 
